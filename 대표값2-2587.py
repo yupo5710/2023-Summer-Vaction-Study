abf44470fd2babfc619b5185eeee9c451881a958
yupo5710 <141139394+yupo5710@users.noreply.github.com>
2023-08-09 23:37:32 +0900

    Delete 대표값2-2587.py

--- 대표값2-2587.py
+++
@@ -1,9 +0,0 @@
-numbers = []
-
-for i in range(5):
-    n = int(input()) #숫자 입력
-    numbers.append(n) #numbers칸에 숫자들 입력
-    
-numbers.sort()
-print(int(sum(numbers)/5)) #평균값 출력
-print(int(numbers[2])) #중앙값 출력
