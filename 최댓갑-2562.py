06da23c7da9318c19f6daa65cc4e0ea3a31b29d3
yupo5710 <141139394+yupo5710@users.noreply.github.com>
2023-08-09 23:33:02 +0900

    Delete 최댓갑-2562.py

--- 최댓갑-2562.py
+++
@@ -1,6 +0,0 @@
-numbers = []
-for _ in range(9):
-    i = int(input())
-    numbers.append(i) #append로 순차적 숫자 넣음
-print(max(numbers)) #최댓갑 찾기
-print(numbers.index(max(numbers))+1) #index는 0부터 시작하니깐 +1 해야함
