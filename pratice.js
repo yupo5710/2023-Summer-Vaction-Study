8953e8a3323afaf1307e63b85da6a0b2b6e79cf8
yupo5710 <141139394+yupo5710@users.noreply.github.com>
2023-08-09 23:54:51 +0900

    수정

--- pratice.js
+++ pratice.js
@@ -1,12 +1,9 @@
-
 var clickCount = 0;
 var clickTextElement = document.getElementById("clickText");
-  
+
 function updateClickCount() {
-    clickCount++; //clickCount라는 변수에 +1씩 더함
-    clickTextElement.textContent = "click: " + clickCount;
+  clickCount++;
+  clickTextElement.textContent = "click: " + clickCount;
 }
-  
+
 clickTextElement.addEventListener("click", updateClickCount);
-; //click이벤트 발생시 updateClickCount 함수 작동
-  
