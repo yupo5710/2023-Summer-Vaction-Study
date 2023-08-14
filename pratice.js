3aa0ca95a3fbdb498433f9fa2080ac08f2fb5dd3
yupo5710 <141139394+yupo5710@users.noreply.github.com>
2023-08-14 21:44:54 +0900

    웹사이트 비번

--- pratice.js
+++ pratice.js
@@ -1,11 +1,21 @@
-var button = document.getElementById("myButton");
+const loginButton = document.getElementById("login-btn");
 
-button.addEventListener("click", function () {
-  if (button.textContent === "확인") {
-    button.textContent = "취소";
-    button.style.backgroundColor = "red";
-  } else {
-    button.textContent = "확인";
-    button.style.backgroundColor = "blue";
-  }
+loginButton.addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // 아이디 혹은 비번이 공란일 경우
+    if (username === "" || password === "") {
+        alert("Please fill in both username and password.");
+        return;
+    }
+
+    // 비밀번호 맞으면
+    if (password === "summervacation") { // 비밀번호
+        alert("Login successful! You can proceed to the next step.");
+        // 다음페이지로 넘기기(빈페이지)
+        window.location.href = "success.html";
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
 });
